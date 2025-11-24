5946ef1fb09f8f03cae2a875886d31850bee83f4
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-24 10:04:20 +0700

    membuat api.js

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const komikController = require('../controllers/komikController');
+const { getKomikById } = require('../services/komikServices');
+const upload = multer({ storage : multer.memorystorage}());
+
+router.post('/komik', upload.single('gambar'), komikController.createKomik);
+router.get('/komik', komikController.getAllKomik);
+router.get('/komik/:id', komikController,getKomikById);
+router.get('/komik/:id', upload.single('gambar'), komikController.updateKomik);
+router.delete('/komik/:id', komikController.deleteKomik);
+
+module.exports = router;
