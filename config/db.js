b75c3b1406e741d14bd3c9be4d930b3a32241208
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-24 09:10:53 +0700

    membuat file db.js untuk koneksi ke database

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const db = require("../models")
+
+async function  connectDatabase(){
+    try{
+        await db.sequelize.authencticate();
+        console.log("Database berhasil terhubung");
+
+        await db.sequelize.sync({ alter: true});
+        console.log("Database tersinkronasi");
+    } catch (err) {
+        console.error("Database gagal terhubung", err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDatabase;
